195fdedd5c50b9be281dc19738651463a09048b3
9l <9l@users.noreply.github.com>
2024-12-26 15:50:22 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Rust Docker image with Alpine-glibc
+This image based on [Rust](https://hub.docker.com/_/rust) official image, and contains glibc to enable proprietary projects compiled against glibc work on Alpine.
+
+This image includes some quirks to make [glibc](https://www.gnu.org/software/libc/)⁠ work side by side with musl libc (default in Alpine Linux). glibc packages for Alpine Linux are prepared by [Sasha Gerrand](https://github.com/sgerrand)⁠ and the releases are published in [sgerrand/alpine-pkg-glibc](https://github.com/sgerrand/alpine-pkg-glibc)⁠ github repo.
+
+If you need to update your libc library cache, use `/usr/glibc-compat/sbin/ldconfig` instead of the usual `/sbin/ldconfig`. You can also use the `LD_LIBRARY_PATH` as on standard libc-based distributions.
+
+## Supported tags
+No `latest` tag, please use specific version tags.
+
+ - `1.83-alpine-glibc`
+
+## Docker Pull Command
+
+```console
+docker pull sirmark/rust:1.83-alpine-glibc
+```
+
+## Usage Example
+Pull and run.
+```console
+$ docker pull sirmark/rust:1.83-alpine-glibc
+$ docker run --rm -it sirmark/rust:1.83-alpine-glibc
+```
+
+Use in your `Dockerfile`, writing something along the lines of the following will compile and run your project:
+```dockerfile
+FROM sirmark/rust:1.83-alpine-glibc
+
+WORKDIR /myapp
+COPY . .
+
+RUN cargo install --path .
+
+CMD ["myapp"]
+```
+Then, build and run the Docker image:
+
+```console
+$ docker build -t my-rust-app .
+$ docker run -it --rm --name my-running-app my-rust-app
+```
---
+++ update-dockerfile.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+rust_versions = [
+    "1.83",
+]
+
+glibc_version = "2.34-r0"
+
+docker_arches = [
+    "linux/amd64",
+    "linux/arm64",
+]
+
+def read_file(file):
+    with open(file, "r") as f:
+        return f.read()
+
+def write_file(file, contents):
+    dir = os.path.dirname(file)
+    if dir and not os.path.exists(dir):
+        os.makedirs(dir)
+    with open(file, "w") as f:
+        f.write(contents)
+
+def update_alpine_gblic():
+    template = read_file("Dockerfile-alpine-glibc.template")
+
+    for version in rust_versions:
+        rendered = template \
+            .replace("%%TAG%%", version) \
+            .replace("%%GLIBC-VERSION%%", glibc_version)
+        write_file(f"{version}/alpine-glibc/Dockerfile", rendered)
+
+def update_ci():
+    file = ".github/workflows/ci.yml"
+    config = read_file(file)
+
+    versions = ""
+    for version in rust_versions:
+        platform = []
+        for arch in docker_arches:
+            platform.append(f"{arch}")
+        platform = ",".join(platform)
+
+        versions += f"          - name: {version}-alpine-glibc{glibc_version}\n"
+        versions += f"            context: {version}/alpine-glibc\n"
+        versions += f"            platforms: {platform}\n"
+        versions += f"            tags: |\n"
+        for tag in rust_versions:
+            versions += f"              {tag}-alpine-glibc\n"
+
+    marker = "#VERSIONS\n"
+    split = config.split(marker)
+    rendered = split[0] + marker + versions + marker + split[2]
+    write_file(file, rendered)
+
+def usage():
+    print(f"Usage: {sys.argv[0]} update|update-ci|update-all")
+    sys.exit(1)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        usage()
+
+    task = sys.argv[1]
+    if task == "update":
+        update_alpine_gblic()
+    elif task == "update-ci":
+        update_ci()
+    elif task == "update-all":
+        update_alpine_gblic()
+        update_ci()
+    else:
+        usage()
